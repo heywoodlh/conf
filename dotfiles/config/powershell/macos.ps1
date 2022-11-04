6906af24c68a127fc3db63cfaec552a42c9eca8c
Spencer Heywood <l.spencer.heywood@protonmail.com>
2022-11-04 09:51:05 -0600

    added Nix on Darwin stuff
    Signed-off-by: Spencer Heywood <l.spencer.heywood@protonmail.com>

--- dotfiles/config/powershell/macos.ps1
+++ dotfiles/config/powershell/macos.ps1
@@ -9,6 +9,17 @@ if ($?)
     }	
 }
 
+## Nix on Darwin stuff
+$env:PATH = "/run/current-system/sw/bin:" + $env:PATH
+
 function darbuild {
     darwin-rebuild switch -I "darwin-config=$HOME/opt/nixos-builds/darwin/darwin-configuration.nix"
 }
+
+$env:__NIX_DARWIN_SET_ENVIRONMENT_DONE = 1
+$env:NIX_PATH = "darwin-config=$env:HOME/.nixpkgs/darwin-configuration.nix:/nix/var/nix/profiles/per-user/root/channels:$env:HOME/.nix-defexpr/channels"
+$env:NIX_SSL_CERT_FILE = "/etc/ssl/certs/ca-certificates.crt"
+$env:XDG_CONFIG_DIRS = "$env:HOME/.nix-profile/etc/xdg:/etc/profiles/per-user/$env:USER/etc/xdg:/run/current-system/sw/etc/xdg:/nix/var/nix/profiles/default/etc/xdg"
+$env:XDG_DATA_DIRS = "$env:HOME/.nix-profile/share:/etc/profiles/per-user/$env:USER/share:/run/current-system/sw/share:/nix/var/nix/profiles/default/share"
+$env:NIX_USER_PROFILE_DIR = "/nix/var/nix/profiles/per-user/$env:USER"
+$env:NIX_PROFILES = "/nix/var/nix/profiles/default /run/current-system/sw /etc/profiles/per-user/$env:USER $env:HOME/.nix-profile"

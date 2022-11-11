cd9125ba4a3f47700963969aa42df63b5c02453c
Spencer Heywood <l.spencer.heywood@protonmail.com>
2022-11-11 11:04:51 -0700

    updated windows stuff
    Signed-off-by: Spencer Heywood <l.spencer.heywood@protonmail.com>

--- dotfiles/config/powershell/windows.ps1
+++ dotfiles/config/powershell/windows.ps1
@@ -1,9 +1,11 @@
+$env:HOME = "${env:HOMEDRIVE}${env:HOMEPATH}"
+
 function mklink { cmd.exe /c mklink.exe $args }
 
-$env:WINFETCH_CONFIG_PATH = "$env:HOME\.config\winfetch\config.ps1"
+$env:WINFETCH_CONFIG_PATH = "$env:HOME\.config\winfetch\config.ps2"
 
 ## Set Komorebi config path
-$Env:KOMOREBI_CONFIG_HOME = "$HOME\.config\komorebi"
+$Env:KOMOREBI_CONFIG_env:HOME = "$env:HOME\.config\komorebi"
 
 ## Add vim to $PATH
 $vim_dir = Get-ChildItem -Path "C:\Program Files\Vim\" -Attribute 'Directory' -ErrorAction silentlycontinue | Select-Object -ExpandProperty FullName | Select -Last 1
@@ -17,4 +19,4 @@ Remove-Alias -Name ls -ErrorAction silentlycontinue
 function ls { get-childitem -path $args[0] | format-wide -property name }
 
 Remove-Item -Path Function:\ssh-unlock -ErrorAction silentlycontinue
-function ssh-unlock { ssh-add ${HOME}\.ssh\id_rsa }
+function ssh-unlock { ssh-add "${env:HOME}\.ssh\id_rsa" }

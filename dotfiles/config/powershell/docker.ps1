075a9af9b8ad36be7e870defcaa7855ae310b615
Spencer Heywood <l.spencer.heywood@protonmail.com>
2022-11-29 11:17:19 -0700

    updated syft function
    Signed-off-by: Spencer Heywood <l.spencer.heywood@protonmail.com>

--- dotfiles/config/powershell/docker.ps1
+++ dotfiles/config/powershell/docker.ps1
@@ -181,7 +181,7 @@ function speedtest() { docker run --rm -it heywoodlh/speedtest-cli $args }
 
 function sublist3r() { docker run -it --rm heywoodlh/sublist3r:latest $args }
 
-function syft() { docker run -v "$((get-location).path):/data" -w /data -it --rm anchore/syft $args }
+function syft() { docker run -v "$((get-location).path):/data" -w /data -i --rm anchore/syft $args }
 
 function takeover() { docker run -it --rm -w /data -v "$((get-location).path):/data" heywoodlh/takeover $args}
 

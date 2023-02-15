2612ccd1968908200b3b3a87ecf6e33d2e6aa2f2
Spencer Heywood <l.spencer.heywood@protonmail.com>
2023-02-15 15:10:59 -0700

    updated powershell with prettified context about aws, kubernetes and git
    Signed-off-by: Spencer Heywood <l.spencer.heywood@protonmail.com>

--- dotfiles/config/powershell/Microsoft.PowerShell_profile.ps1
+++ dotfiles/config/powershell/Microsoft.PowerShell_profile.ps1
@@ -17,47 +17,126 @@ if (test-path ~/bin) {
 }
 
 # Check if tmux session
-if (dir env:TMUX) {
+if (dir env:TMUX -erroraction silentlycontinue) {
     $isTmux = 'true'
 }
 
+## Function to compare terminal width to a string and append that string to the prompt
+
 ## Create pure-looking prompt
 function prompt {
+    # Store exit code of previous command for prompt color
+    $exit_code = $global:LASTEXITCODE
+    $global:LASTEXITCODE = $exit_code
+
+    # Use PWD for Tmux stuff and update the PWD environment variable
     $global:OLDPWD = $global:PWD
     $global:PWD = $(Get-Location -PSProvider FileSystem).ProviderPath
     $env:PWD = $global:PWD
     if ($isTmux -eq 'true') {
         tmux setenv -g PWD $env:PWD
     }
-    $exit_code = $global:LASTEXITCODE
-
-    if (git rev-parse --is-inside-work-tree) {
-	$git_branch = $(git branch --show-current)
-    } else {
-	$git_branch = ''
+   
+    # If git is installed
+    if (get-command git -erroraction silentlycontinue) { 
+        # If inside git repo
+        if (git rev-parse --is-inside-work-tree) {
+            # Get git branch
+            $git_branch = $(git branch --show-current)
+        } else {
+            $git_branch = ''
+        }
     }
 
+    # If kubectl is installed
+    if (get-command kubectl -erroraction silentlycontinue) { 
+        $kubectl_context = kubectl config current-context
+    }
+
+    # Get current terminal_width
+    $terminal_width = (Get-Host).UI.RawUI.BufferSize.Width
+    
+    # Replace $HOME with ~ in working_dir (and "\" with "/" if on Windows)
     $home_directory = $env:HOME
     $working_dir = (get-location).Path
-    $terminal_width = (Get-Host).UI.RawUI.BufferSize.Width
-    $terminal_width = $terminal_width - 10
-
     if ($working_dir.contains($home_directory)) {
         $working_dir = $working_dir.Replace($HOME, "~")
     }
+    if ($isWindows) {
+        $working_dir = $working_dir.Replace("\", "/")
+    }
 
-    if ($working_dir.Length -gt $terminal_width) {
+    # If working_dir is longer than terminal_width, use short working_dir
+    if (${working_dir}.Length -gt $terminal_width) {
         $working_dir = "./$(Split-Path -leaf -path (Get-Location))"
     }
 
-    $global:LASTEXITCODE = $exit_code
-    ## If last command succeeded, show white prompt, otherwise show red
+    ## If last command succeeded
     if ( $exit_code -eq 0 ) {
-	"$([char]27)[34m${working_dir} $([char]27)[92m${git_branch}`r`n$([char]27)[37m❯ "
-    } else
-    {
-	"$([char]27)[34m${working_dir} $([char]27)[92m${git_branch}`r`n$([char]27)[31m❯ "
+        ## Set prompt_cursor_color to white
+        $prompt_cursor_color = "37m"
+    } else {
+        ## Set prompt_cursor_color to red, when last command failed
+        $prompt_cursor_color = "31m"
     }
+
+    # Values that will always be visible in prompt
+    $working_dir_prompt = "$([char]27)[34m${working_dir}"
+    $prompt_cursor = "$([char]27)[$prompt_cursor_color❯ "
+
+    # Array to add optional values to
+    $optional_prompt_values = @()
+    # Values that may or may not be visible in my prompt, based on terminal width
+    # (Make sure that checks for null or empty values are here)
+    if ($git_branch -ne $null -and $git_branch -ne '') {
+        $git_branch_prompt = "$([char]27)[92m${git_branch}"
+        $optional_prompt_values += $git_branch_prompt
+    }
+    if ($env:AWS_PROFILE) {
+        $aws_profile_prompt = "$([char]27)[33m${env:AWS_PROFILE}"
+        $optional_prompt_values += $aws_profile_prompt
+    }
+    if ($kubectl_context -ne $null -and $kubectl_context -ne '') {
+	# Get the last string after "/" character in AWS context to clean up
+	if ($kubectl_context.contains("arn:aws")) {
+            $kubectl_context = $kubectl_context.split('/')[1]
+	}
+        $kubectl_context_prompt = "$([char]27)[36m${kubectl_context}"
+        $optional_prompt_values += $kubectl_context_prompt
+    }
+
+    # Define line_1 to append desired values shown in prompt, based on terminal width
+    $line_1 = "${working_dir_prompt}"
+
+    # Define function to append stuff to line_1
+    function append_to_prompt {
+        param(
+            [string]$prompt_line,
+            [string]$new_value,
+            [int]$max_length
+        )
+	# Only move forward if new_value is not empty
+	if ($new_value -ne $Null -and $new_value -ne '') {
+            # Check if adding the new value will make the prompt too long
+            if (($prompt_line.Length + $new_value.Length + 1) -le $max_length) {
+                # Append the new value to the prompt line
+                if ($prompt_line.Length -gt 0) {
+                    # Add a separator character if the prompt line is not empty
+                    $prompt_line += " "
+                }
+                $prompt_line += $new_value
+            }
+        }	
+        return $prompt_line
+    }
+
+    # For each optional value, append it to line_1 if there is room
+    foreach ($value in $optional_prompt_values) {
+        $line_1 = append_to_prompt $line_1 $value $terminal_width
+    }
+
+    $prompt = "$line_1 `r`n$prompt_cursor"
+    return $prompt
 }
 
 ## Function for installing/loading modules

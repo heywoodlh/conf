3fed34f6980352f27e8c6a229a73c68bf59f1322
Spencer Heywood <l.spencer.heywood@protonmail.com>
2023-02-20 19:32:04 -0700

    updated vultr-unlock function
    Signed-off-by: Spencer Heywood <l.spencer.heywood@protonmail.com>

--- dotfiles/config/powershell/functions/vultr.ps1
+++ dotfiles/config/powershell/functions/vultr.ps1
@@ -2,7 +2,7 @@ function vultr-unlock {
     if (!$env:VULTR_API_KEY) {
         $env:VULTR_API_KEY = bw get password 2eb34e09-f5b4-4fc2-9c65-ace7013dd1b4
     }
-    $env:TF_VAR_VULTR_API_KEY = $env:VULTR_API_KEY 
+    $env:TF_VAR_vultr_api_key = $env:VULTR_API_KEY 
 }
 
 function vultr-cli {

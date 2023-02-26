888c9c446306dc05c79509e503ed0e6ca85b1149
Spencer Heywood <l.spencer.heywood@protonmail.com>
2023-02-25 18:12:28 -0700

    updated Dockerfile and scripts with smaller layers and fixed kubectl
    Signed-off-by: Spencer Heywood <l.spencer.heywood@protonmail.com>

--- .devcontainer/docker/scripts/kubectl.sh
+++ .devcontainer/docker/scripts/kubectl.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-apt-get update && apt-get install -y curl && rm -rf /var/lib/apt/lists/*
-
-curl -L "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/$(arch)/kubectl" -o /usr/local/bin/kubectl
+apt-get update && apt-get install -y ca-certificates curl 
+curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys B53DC80D13EDEF05
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
+apt-get update && apt-get install -y kubectl
+rm -rf /var/lib/apt/lists/*

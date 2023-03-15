23044ca47ca16870fb2ccbc76b423d350989c3b4
Spencer Heywood <l.spencer.heywood@protonmail.com>
2023-03-15 16:07:00 -0600

    using chocolatey to install packages
    Signed-off-by: Spencer Heywood <l.spencer.heywood@protonmail.com>

--- dependencies/windows/install.ps1
+++ dependencies/windows/install.ps1
@@ -22,29 +22,6 @@ write-output "chocolatey is installed, continuing with installing choco packages
 
 Start-Process powershell -Verb RunAs -ArgumentList "choco install ${current_directory}\packages.config --yes --acceptlicense"
 
-# Install winget if not installed
-if (-not (get-command winget.exe -errorAction SilentlyContinue)) {
-    Write-Host "Installing winget"
-    # get latest download url
-    $url = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
-    $url = (Invoke-WebRequest -Uri $url).Content | ConvertFrom-Json |
-            Select-Object -ExpandProperty "assets" |
-            Where-Object "browser_download_url" -Match '.msixbundle' |
-            Select-Object -ExpandProperty "browser_download_url"
-    # download
-    Invoke-WebRequest -Uri $url -OutFile "${current_directory}\Setup.msix" -UseBasicParsing
-    # install
-    Start-Process powershell -Verb RunAs -ArgumentList "Add-AppxPackage -Path ${current_directory}\Setup.msix"
-    # delete file
-    Remove-Item "${current_directory}\Setup.msix"
-    # Wait until winget is installed
-    check_command winget.exe
-}
-
-# Install winget packages
-Start-Process powershell -Verb RunAs -ArgumentList "winget import -i ${current_directory}\winget.json --ignore-unavailable --accept-package-agreements --accept-source-agreements"
-
-# Setup Windows Terminal config
 new-item -itemtype directory -path "$HOME\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState" -erroraction silentlycontinue
 copy-item -v ${current_directory}\..\..\dotfiles\config\windows-terminal\settings.json "$HOME\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
 

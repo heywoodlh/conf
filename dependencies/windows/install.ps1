e1ed9f50bbf6965c0b9f6a24d4abede8829f8d4c
Spencer Heywood <l.spencer.heywood@protonmail.com>
2023-03-16 10:49:45 -0600

    set Windows Terminal to be symlink
    Signed-off-by: Spencer Heywood <l.spencer.heywood@protonmail.com>

--- dependencies/windows/install.ps1
+++ dependencies/windows/install.ps1
@@ -11,6 +11,19 @@ function check_command () {
     write-output "$command is installed"
 }
 
+function test-symlink($filePath) {
+    # For Windows check if file is symlink using dir
+    if ($isWindows) {
+        (Get-ItemProperty "${filePath}").LinkType 2>$null
+        $global:symcheck_code = $?
+    } 
+    # For everything not Windows, use BASH to check symlink
+    else {
+        bash -c "test -L ${filePath}"
+        $global:symcheck_code = $?
+    }
+}
+
 if (-not (get-command choco.exe -errorAction SilentlyContinue)) {
     Write-Host "Installing Chocolatey"
     Start-Process -Wait powershell -Verb RunAs -ArgumentList '-Command "Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString(''https://chocolatey.org/install.ps1''))"'
@@ -23,6 +36,13 @@ write-output "chocolatey is installed, continuing with installing choco packages
 
 Start-Process -Wait powershell -Verb RunAs -ArgumentList "choco install ${current_directory}\packages.config --yes --acceptlicense"
 
+# Create symlink for Windows Terminal settings
+if (! test-symlink $HOME\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json) {
+    if (test-path $HOME\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json) {
+        move-item $HOME\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json $HOME\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json.bak
+        new-item -itemtype SymbolicLink -path $HOME\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json -target "${dotfiles_directory}\dotfiles\config\windows-terminal\settings.json"
+    }
+}
 new-item -itemtype directory -path "$HOME\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState" -erroraction silentlycontinue
 copy-item -v ${current_directory}\..\..\dotfiles\config\windows-terminal\settings.json "$HOME\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
 

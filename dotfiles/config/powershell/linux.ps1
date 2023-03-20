16fbb75f377141f545cd12c2198452c896679b76
Spencer Heywood <l.spencer.heywood@protonmail.com>
2023-03-20 10:35:30 -0600

    start ssh agent on ssh connection
    Signed-off-by: Spencer Heywood <l.spencer.heywood@protonmail.com>

--- dotfiles/config/powershell/linux.ps1
+++ dotfiles/config/powershell/linux.ps1
@@ -45,3 +45,10 @@ if ($isNixOS)
     }
 
 }
+
+# Start ssh agent if connected over SSH
+if ($env:SSH_CONNECTION) {
+    $ssh_agent_output = $(ssh-agent -s)
+    $env:SSH_AUTH_SOCK = $ssh_agent_output | grep SSH_AUTH_SOCK | cut -d "=" -f 2 | cut -d ";" -f1
+    $env:SSH_AGENT_PID = $ssh_agent_output | grep SSH_AGENT_PID | cut -d "=" -f 2 | cut -d ";" -f1
+}

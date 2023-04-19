4e9f92609ba5b2b6d844f86d86cad4817c90fd3a
Spencer Heywood <l.spencer.heywood@protonmail.com>
2023-04-19 09:55:08 -0600

    cross-platform alacritty config
    Signed-off-by: Spencer Heywood <l.spencer.heywood@protonmail.com>

---
+++ dotfiles/config/alacritty/launch.ps1
@@ -0,0 +1,10 @@
+#!/usr/bin/env pwsh
+
+# PowerShell script for launching stuff with Alacritty
+# Launch Tmux on Linux/MacOS, don't on Windows
+if (-not ($isWindows)) {
+    $env:PATH = "/etc/profiles/per-user/$env:USER/bin:/run/current-system/sw/bin:$env:PATH"
+    tmux
+} else {
+    pwsh
+}

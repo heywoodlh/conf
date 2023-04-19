41f00c44364650469a2fd756e9d53b08ac293cd9
Spencer Heywood <l.spencer.heywood@protonmail.com>
2023-04-19 10:44:17 -0600

    fixed font on tmux launch
    Signed-off-by: Spencer Heywood <l.spencer.heywood@protonmail.com>

--- dotfiles/config/alacritty/launch.ps1
+++ dotfiles/config/alacritty/launch.ps1
@@ -4,6 +4,7 @@
 # Launch Tmux on Linux/MacOS, don't on Windows
 if (-not ($isWindows)) {
     $env:PATH = "/etc/profiles/per-user/$env:USER/bin:/run/current-system/sw/bin:$env:PATH"
+    $env:TERM = "screen-256color"
     tmux
 } else {
     pwsh
